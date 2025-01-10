169fe333cb5d7fd1ef32b0eaf82b7f124f05bbba
JM-Na <myung0484@gmail.com>
2025-01-10 20:38:50 +0900

    스케줄러 위치 변경

--- src/main/java/jmna/loacalc/scheduler/loaApiScheduler.java
+++ src/main/java/jmna/loacalc/scheduler/loaApiScheduler.java
@@ -1,4 +1,4 @@
-package jmna.scheduler;
+package jmna.loacalc.scheduler;
 
 import jmna.loacalc.processor.auction.AuctionProcessor;
 import jmna.loacalc.processor.market.MarketProcessor;
@@ -13,10 +13,21 @@ public class loaApiScheduler {
     private final AuctionProcessor auctionProcessor;
     private final MarketProcessor marketProcessor;
 
+    /**
+     * 시각이 아닌 시간 단위 작업은 서버 로딩시 기준으로 바로 실행되고 계산됨.
+     */
+
+
     // 매시간 실행 (1시간 간격)
     @Scheduled(fixedRate = 3600000) // 1시간 = 3600000ms
-    public void executeTask() {
-        System.out.println("작업 실행 중: " + java.time.LocalDateTime.now());
+    public void executeTaskByHour() {
+        System.out.println("1시간당 작업 실행 중: " + java.time.LocalDateTime.now());
+        // 여기에 실행할 작업 로직 추가
+    }
+
+    @Scheduled(fixedRate = 60000) // 1시간 = 3600000ms
+    public void executeTaskByMinute() {
+        System.out.println("1분당 작업 실행 중: " + java.time.LocalDateTime.now());
         // 여기에 실행할 작업 로직 추가
     }
 
